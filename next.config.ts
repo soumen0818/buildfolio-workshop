f1bc0a3700d921e8409ed82f25ee0779b4d7aede
Soumen Mandal <soumenmandal1080@gmail.com>
2025-11-11 23:29:02 +0530

    fix error

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'www.soumendas.me',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "www.soumendas.me",
+        pathname: "/images/**",
       },
     ],
   },
